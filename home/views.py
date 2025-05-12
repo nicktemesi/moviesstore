957313c78783b8fca475b2aa3c2305da4f9db40c
Peter <peters.oluoch@gmail.com>
2025-05-12 13:05:13 +0300

    Updated routes home views

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from flask import request_tearing_down
 
 # Create your views here.
+
+def index(request):
+    return render(request, index.html)
+
+def about(request):
+    return render(request, 'home/about.html')
