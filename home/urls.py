d670f4588104d8bb0f7b6e96e692cda8647c9f52
Bony Born <ezekielbon2@gmail.com>
2025-05-20 21:25:28 +0000

    privacy policy page on homepage and landing page

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
 ]
--- home/views.py
+++ home/views.py
@@ -8,3 +8,7 @@ def index(request):
 def about(request):
     template_data = {'title': 'About Us'}
     return render(request, 'home/about.html', template_data)
+
+def privacy_policy(request):
+    template_data = {'title': 'Privacy Policy'}
+    return render(request, 'privacy_policy.html', template_data)
